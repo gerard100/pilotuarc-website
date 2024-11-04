b7f4367560e63c52b3456c669a44dd71826070f2
gerard100 <123904524+gerard100@users.noreply.github.com>
2024-11-05 02:17:02 +1000

    Update script.js

--- Script and Styles/script.js
+++ Script and Styles/script.js
@@ -59,3 +59,19 @@ function toggleDropdown() {
             }
         }
     }
+
+
+const dialog = document.getElementById('dialog');
+const openDialogButton = document.getElementById("openDialog");
+const closeDialogButton = document.getElementById('closeDialog');
+
+openDialogButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+closeDialogButton.addEventListener('click', () => {
+    dialog.close();
+});
+
+
+
+
