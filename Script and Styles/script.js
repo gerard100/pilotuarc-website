d70165de16e0d6c9af53232c677b89bd45781810
gerard100 <123904524+gerard100@users.noreply.github.com>
2024-11-05 11:12:29 +1000

    Update script.js

--- Script and Styles/script.js
+++ Script and Styles/script.js
@@ -87,3 +87,31 @@ function toggleDropdown() {
             }
         }
     }
+
+const form = document.querySelector('form');
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(form);
+
+            fetch(form.action, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert('Thank you for your submission!');
+                    form.reset();
+                } else {
+                    alert('Oops! There was a problem submitting your form.');
+                }
+            })
+            .catch(error => {
+                alert('Oops! There was a problem submitting your form.');
+            });
+        });
+
+
+
